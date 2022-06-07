b50e3179e1f895b62ac19a66c7c7e50ee443a3dd
clarent <d.artagnan087@gmail.com>
2022-06-08 01:25:00 +0900

    6/8

--- algorithm/exam/ch14/test.py
+++ algorithm/exam/ch14/test.py
@@ -17,3 +17,29 @@ print(b)
 
 a = None > 1
 print(a)
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(reversed=True)
+
+
+a = 0b0011
+b = 0b11
+
+print(a)
+print(b)
+
+x = 1
+y = 4
+z = bin( x^y)
+print(z)
+print(type(z))
+
+zz= '0b101'
+zz.count('1')
+
+bin(-1 & 0xFFFFFFFF)
